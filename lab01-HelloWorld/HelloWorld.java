853200933b2351b9c3205ff452fe44f03da24159
kks219 <kks219@lehigh.edu>
2015-08-28 15:42:17 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+//Kelly Skewis
+//Hello World Java Program
+
+// define a class
+public class HelloWorld{
+    
+// add main method
+    public static void main(String [ ] args ){
+        
+// print the statement with a box around it
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2015!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("testing");
+    }
+}

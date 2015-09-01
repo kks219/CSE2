548a271e69901aa9a834c24dd4e93207ca4cc906
kks219 <kks219@lehigh.edu>
2015-09-01 19:22:10 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Kelly Skewis
+//August 28, 2015
+//CSE2 section 110
+
+// define class
+public class WelcomeClass{
+    
+// main method
+    public static void main (String [ ] agrs){
+        
+//print out statement
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-K--K--S--2--1--9->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I have a 16 year-old little brother, a cat, and a dog.");
+    }
+}
